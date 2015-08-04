3c3ef72bcdf3fac1fa18c3d0e6831d1e4f6ec451
Jamie Simper <fromraintohope@gmail.com>
2015-08-04 00:22:04 -0400

    removed duplicate file

--- browser.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var Photo = require('./Photo.react.js');
-
-var browser = function() {
-  var div = document.querySelector('[data-js="wrapper"]');
-  React.render(<Photo />, div);
-};
-
-module.exports = browser;
