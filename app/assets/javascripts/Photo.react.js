62957e78152715408bc4922ab9bd4dcaa0c18d4b
Jamie Simper <fromraintohope@gmail.com>
2015-08-04 00:18:32 -0400

    added some flair

---
+++ app/assets/javascripts/Photo.react.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+
+var Photo = React.createClass({
+
+  render: function() {
+    return <img src='http://media.giphy.com/media/mNBipKRNuymnC/giphy.gif' />
+  }
+});
+
+module.exports = Photo;
